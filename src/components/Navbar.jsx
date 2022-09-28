c41328019187f46667efc2bf0eaf20a1dfca5946
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-28 19:02:51 +0100

    feats: added components + router

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <div>
+                    DYC
+                </div>
+                <div>
+                    NavLinks
+                </div>
+            </nav>
+        </div>
+    )
+}
