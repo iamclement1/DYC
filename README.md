8a2e58369177b5c19accf20eee50ce485f477498
Nnamdi Clement <blessedmarcel1@gmail.com>
2022-09-28 18:56:58 +0100

    feats: removed default files

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 ## Welcome to DYC 2022
-
-`` This is an event landing page for DYC 2022 ``
--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1 className="text-4xl font-bold">
-        Welcome to DYC  2022
+        Welcome to DYC 2022
       </h1>
     </div>
   )
