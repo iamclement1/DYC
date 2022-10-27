b5c7122be16e1812d3e12039eb0fe902f8d248d7
iamclement1 <blessedmarcel1@gmail.com>
2022-10-27 23:08:57 +0100

    feats: added about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,24 @@
+import Img from '../assets/dyc2.png'
+
+export const About = () => {
+    return (
+        <main>
+            <div className="mx-auto text-center">
+                <img src={Img} alt="About-Us-Image" className="mx-auto md:mt-20 mt-12 md:w-[75%] w-[85%]" />
+                <h1 class="md:mt-12 mt-4 text-[#5C407F] uppercase font-bold md:text-4xl text-xl mb-4">About Us</h1>
+                <div className=" px-40">
+                    <p className="text-sm">
+                        Diocesan Youth Convention (DYC) is an annual camp meeting organized by the Diocesan Youth Department of the Ibadan Anglican Diocese. It has provided unique spiritual learning opportunities since 2000 for Christian youths.
+                    </p>
+                    <p className="text-sm py-8">
+                        A 4-day camp meeting for youth will be offered this year by DYC, designed specifically to help them develop the faith they need to face today's challenges. From 18th to 22nd December, the convention will be held at the United Missionary Comprehensive College (UMCC), Molete, Ibadan, Nigeria.
+                    </p>
+                    <p className="text-sm">
+                        The DYC provides campers with the opportunity to explore their faith, develop their relationship with God, and form friendships with other young people.
+                    </p>
+
+                </div>
+            </div>
+        </main>
+    )
+}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import { About } from './About'
 
 export default function Landing() {
     return (
         <div>
             <Hero />
-            About us
+            <About />
         </div>
     )
 }
