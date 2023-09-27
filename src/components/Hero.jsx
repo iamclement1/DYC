f877546689b44991824ac724d59ffe253ee9ca31
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-09-27 11:10:50 +0100

    feat: made navbar transparent and added more context to the hero component :fire:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,25 +1,29 @@
-import IMG1 from '../assets/hero.png'
-import Button from './Button'
+
 
 export default function Hero() {
     return (
-        <div className="md:mt-12 lg:mt-12 xl:mt-12 mt-20 md:py-32 py-6 hero h-screen text-center text-white">
-            <div className="md:py-32 py-6 mb-0" >
-                <h2 className="md:text-5xl text-md font-bold uppercase md:mb-6 ">
+        <div className=" md:py-32 py-6 hero h-screen text-center text-white">
+            <div className="md:py-60 py-6 mb-0" >
+                <h1 className="md:text-3xl text-md font-bold uppercase md:mb-7">
+                    Welcome to
+                </h1>
+                <h2 className="md:text-6xl text-md font-bold uppercase md:mb-6 ">
                     diocesan youth department
                 </h2>
                 <span className=" md:py-6 text-center text-white md:text-2xl text-sm font-semibold uppercase block">
-                    #dyc'22
+                    #dyc'23
                 </span>
-                <button className="bg-[#ff9500] px-3 py-2 text-sm rounded-sm text-white  uppercase
-                            hover:bg-[#eea135] hover:border hover:text-white transition ease-in-out duration-300 "
-                    onClick={() => window.location.href = "https://forms.gle/BeJidbUiXhbLpcDU9"}>
-                    register
-                </button>
-
+                <a href="https://forms.gle/PTanyCkFyWJ5ptqS7" target="_blank">
+                    <button className="bg-[#ff9500] px-3 py-2 text-sm rounded-sm text-white  uppercase
+                            hover:bg-[#eea135] hover:border hover:text-white transition ease-in-out duration-300 ">
+                        register
+                    </button>
+                </a>
             </div>
         </div>
     )
 }
+
+// onClick = {() => window.location.href = "https://forms.gle/PTanyCkFyWJ5ptqS7"}
 
 // top-28
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,39 @@
-import { Link } from "react-router-dom";
-import Button from './Button'
 import Logo from '../assets/Diocesan-Logo.svg'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaBars } from 'react-icons/fa'
 
 
 export default function () {
     const [open, setOpen] = useState(false)
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const scrollThreshold = 500; // Adjust this threshold as needed
+
+            if (scrollY > scrollThreshold) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
-            <nav className="bg-white shadow-md fixed inset-x-0 top-0 z-10 w-full mb-5 ">
+            <nav className={`bg-${scrolled ? 'white' : "transparent"} text-${scrolled ? 'black' : 'white'} fixed inset-x-0 top-0 z-10 w-full mb-5 `}>
                 <div className="flex items-center text-sm justify-between md:pr-20">
                     <div className="z-50 p-3 md:w-auto w-full flex justify-between items-center">
                         <img src={Logo} alt="brand__logo" className="md:ml-12 
-                        md:w-full w-1/5 border border-radius border-purple-600 rounded-full"/>
+                        md:w-full md:h-full w-5 h-5 border border-radius border-purple-600 rounded-full"/>
                         <div className="text-2xl md:hidden items-center" onClick={() => setOpen(!open)}>
                             <FaBars name={`${open ? "close" : "menu"}`} />
                         </div>
@@ -21,7 +41,7 @@ export default function () {
                     <ul className="md:flex  font-semibold hidden uppercase items-center md:mr-6">
                         <li>
                             <a href="#about" className="py-7 px-3 inline-block md:mr-4 hover:text-[#5C407F]">
-                                about dyc 22
+                                about dyc 23
                             </a>
                             <a href="#sponsor" className="py-7 px-3 inline-block md:mr-4 hover:text-[#5C407F]">
                                 support us
@@ -29,9 +49,9 @@ export default function () {
                             <a href="#about-dyd" className="py-7 px-3 inline-block md:mr-4 hover:text-[#5C407F]">
                                 about dyd
                             </a>
-                            <a href="#speakers" className="py-7 px-3 inline-block md:mr-4 hover:text-[#5C407F]">
+                            {/* <a href="#speakers" className="py-7 px-3 inline-block md:mr-4 hover:text-[#5C407F]">
                                 meet our ministers
-                            </a>
+                            </a> */}
                         </li>
                         {/* <div className="md:flex md:space-x-7 hidden">
                             <Button Text="Donate" />
@@ -56,11 +76,11 @@ export default function () {
                                 about dyd
                             </a>
                         </li>
-                        <li className="">
+                        {/* <li className="">
                             <a href="#speakers" className="py-3 hover:text-[#5C407F] px-12 inline-block">
                                 meet our ministers
                             </a>
-                        </li>
+                        </li> */}
                         {/* <div className="px-12 py-5">
                             <Button Text="Donate" />
                         </div> */}
